4f28d896b111272c7b54bf1543274020aedec58d
Raphael Magalhães <raphael.sdm@gmail.com>
2020-06-23 08:39:14 -0300

    Add initial files with FullScreenDialog demo

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const FullScreenDialog = () => {
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (window.localStorage.getItem("modal") === "true") {
+      setOpen(true);
+    }
+  }, [open]);
+
+  const handleClickOpen = () => {
+    window.localStorage.setItem("modal", true);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    window.localStorage.setItem("modal", false);
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <div style={{ padding: "20px" }}>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Open full-screen dialog
+        </Button>
+      </div>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText
+              primary="Default notification ringtone"
+              secondary="Tethys"
+            />
+          </ListItem>
+        </List>
+      </Dialog>
+    </div>
+  );
+};
+
+export default FullScreenDialog;
